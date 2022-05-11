5922412955d48d7b284dadfd0e40a2db63febda2
Arthur <arthurdutoit.conatct@gmail.com>
2022-05-11 09:59:40 +0200

    Mouvemens fonctionnels mais pas collisions

--- player.py
+++ player.py
@@ -10,9 +10,40 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey([0, 0, 0])
         self.rect = self.image.get_rect()
         self.position = [x, y]
+        self.images = {
+            'down': self.get_image(0, 16),
+            'left': self.get_image(32, 16),
+            'right': self.get_image(48, 16),
+            'up': self.get_image(16, 16),
+        }
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
+        self.speed = 2
+
+    def save_location(self):
+        self.old_position = self.position.copy()
+
+    def change_animation(self, name):
+        self.image = self.images[name]
+        self.image.set_colorkey ([0, 0, 0])
+
+    def move_right(self): self.position[0] += self.speed
+
+    def move_left(self): self.position[0] -= self.speed
+
+    def move_up(self): self.position[1] -= self.speed
+
+    def move_down(self): self.position[1] += self.speed
 
     def update(self):
         self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def move_back(self):
+        self.position = self.old_position
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
 
     def get_image(self, x, y):
         image = pygame.Surface([16, 16])
