788f46e80faf4f6b0d176f0ea0fa85932c920a27
Arthur <arthurdutoit.conatct@gmail.com>
2022-05-12 17:16:18 +0200

    House_1UP finished (collisions + spawn)

---
+++ Jeu de tuiles/Maison_1UP/TilesetHouseTrans.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TilesetHouseTrans" tilewidth="16" tileheight="16" tilecount="667" columns="29">
+ <image source="../../../../Desktop/NinjaAdventure/Backgrounds/Tilesets/TilesetHouse.png" trans="000000" width="464" height="368"/>
+</tileset>
---
+++ Jeu de tuiles/Maison_1UP/TilesetInterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TilesetInterior" tilewidth="16" tileheight="16" tilecount="320" columns="16" backgroundcolor="#898989">
+ <image source="../../../../Desktop/projet prog/NinjaAdventure/Backgrounds/Tilesets/Interior/TilesetInterior.png" trans="ffffff" width="256" height="320"/>
+</tileset>
